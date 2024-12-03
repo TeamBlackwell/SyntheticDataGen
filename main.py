7d0443dfd2d9b76e35e88dc52cf6f8073ccbd1c5
Nikhil Henry <25770025+nikhilhenry@users.noreply.github.com>
2024-12-03 21:19:46 +0530

    refactor to support multiple files

--- main.py
+++ main.py
@@ -7,28 +7,44 @@ from building_data import batch_export
 from pathlib import Path
 
 
+def generate_cityscapes(args):
+    path = Path(args.output_dir)
+    if not path.exists():
+        path.mkdir(parents=True)
+
+    batch_export(path, args.num_cities, args.map_size, args.prefix)
+
+
 def main():
-    parser = argparse.ArgumentParser(description="Generate data for cityscapes")
-    parser.add_argument(
+    parser = argparse.ArgumentParser(
+        description="Generate synthetic data for predicting local wind fields"
+    )
+    subprasers = parser.add_subparsers(dest="command")
+
+    # Cityscapes
+    cityscapes_parser = subprasers.add_parser(
+        "cityscapes", help="Generate cityscape csv files"
+    )
+
+    cityscapes_parser.add_argument(
         "--num_cities", type=int, default=60, help="Number of cityscapes to generate"
     )
-    parser.add_argument("--map_size", type=int, default=250, help="Size of the map")
+    cityscapes_parser.add_argument(
+        "--map_size", type=int, default=250, help="Size of the map"
+    )
     parser.add_argument(
         "--output_dir",
         type=str,
         default="data/cityscapes",
         help="Directory to save buildings",
     )
-    parser.add_argument(
+    cityscapes_parser.add_argument(
         "--prefix", type=str, default="city", help="Prefix for the output files"
     )
     args = parser.parse_args()
 
-    path = Path(args.output_dir)
-    if not path.exists():
-        path.mkdir(parents=True)
-
-    batch_export(path, args.num_cities, args.map_size, args.prefix)
+    if args.command == "cityscapes":
+        generate_cityscapes(args)
 
 
 if __name__ == "__main__":
