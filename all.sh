24cde931c97fe576222342fa7aac8a7954c0e293
Nikhil Henry <25770025+nikhilhenry@users.noreply.github.com>
2024-12-08 22:51:24 +0530

    add all script

---
+++ all.sh
@@ -0,0 +1,6 @@
+python3 main.py gen cityscapes
+python3 main.py gen windflow
+python3 main.py gen drone
+python3 main.py viz wind --index 10 --export-all
+python3 main.py viz wind --index 10 --export-all-transparent --export-dir data/transparent
+python3 main.py gen lidar
